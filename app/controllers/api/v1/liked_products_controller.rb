248109134d1613404cf7720fb6cf028fcf11c768
AnushatRently <anushkumar.s@rently.com>
2021-07-28 10:54:59 +0530

    done

--- app/controllers/api/v1/liked_products_controller.rb
+++ app/controllers/api/v1/liked_products_controller.rb
@@ -5,7 +5,7 @@ module Api
 
   def index
     @user = User.find(params[:id])
-    if (@user.id.to_i == current_user.id.to_i and @user.role == 1)
+    if (@user.id == current_user.id and @user.role == 1)
       @liked_products = @user.liked_products
       render json: @liked_products,only:[:id,:title,:description,:cost,:user_id,:available],status: :ok
     else
---
+++ app/controllers/api/v1/likes_controller.rb
@@ -0,0 +1,9 @@
+module Api
+  module V1
+    class LikesController < Api::V1::ApplicationController
+
+
+
+    end
+  end
+end
